f889a2b58db92fa460a8e4f2608a555c9b164846
Noamancy <69311684+slave2midna@users.noreply.github.com>
2026-04-15 08:32:53 -0500

    Update companion-sheet.mjs

--- module/companion-sheet.mjs
+++ module/companion-sheet.mjs
@@ -1,10 +1,23 @@
-export class CompanionSheet extends foundry.applications.sheets.ActorSheetV2 {
+const { ActorSheetV2 } = foundry.applications.sheets;
+const { HandlebarsApplicationMixin } = foundry.applications.api;
+
+export class CompanionSheet extends HandlebarsApplicationMixin(ActorSheetV2) {
   static DEFAULT_OPTIONS = foundry.utils.mergeObject(super.DEFAULT_OPTIONS, {
     classes: ["deepshadow", "sheet", "actor", "companion-sheet"],
     position: {
       width: 780
+    },
+    form: {
+      closeOnSubmit: false
     }
   }, { inplace: false });
+
+  static PARTS = {
+    sheet: {
+      root: true,
+      template: "systems/deepshadow/templates/companion-sheet.hbs"
+    }
+  };
 
   get title() {
     return this.actor?.name || "Companion";
@@ -12,14 +25,8 @@ export class CompanionSheet extends foundry.applications.sheets.ActorSheetV2 {
 
   async _prepareContext(options) {
     const context = await super._prepareContext(options);
-
     context.actor = this.actor;
     context.levelLabel = "RP";
-
     return context;
-  }
-
-  async _renderHTML(context, options) {
-    return renderTemplate("templates/companion-sheet.hbs", context);
   }
 }
