d14eb1c7881ee07a07a5e43f6c5d30e55f9064d4
Noamancy <69311684+slave2midna@users.noreply.github.com>
2026-04-15 08:32:44 -0500

    Update ranger-sheet.mjs

--- module/ranger-sheet.mjs
+++ module/ranger-sheet.mjs
@@ -1,10 +1,23 @@
-export class RangerSheet extends foundry.applications.sheets.ActorSheetV2 {
+const { ActorSheetV2 } = foundry.applications.sheets;
+const { HandlebarsApplicationMixin } = foundry.applications.api;
+
+export class RangerSheet extends HandlebarsApplicationMixin(ActorSheetV2) {
   static DEFAULT_OPTIONS = foundry.utils.mergeObject(super.DEFAULT_OPTIONS, {
     classes: ["deepshadow", "sheet", "actor", "ranger-sheet"],
     position: {
       width: 780
+    },
+    form: {
+      closeOnSubmit: false
     }
   }, { inplace: false });
+
+  static PARTS = {
+    sheet: {
+      root: true,
+      template: "systems/deepshadow/templates/ranger-sheet.hbs"
+    }
+  };
 
   get title() {
     return this.actor?.name || "Ranger";
@@ -12,14 +25,8 @@ export class RangerSheet extends foundry.applications.sheets.ActorSheetV2 {
 
   async _prepareContext(options) {
     const context = await super._prepareContext(options);
-
     context.actor = this.actor;
     context.levelLabel = "LV";
-
     return context;
-  }
-
-  async _renderHTML(context, options) {
-    return renderTemplate("templates/ranger-sheet.hbs", context);
   }
 }
