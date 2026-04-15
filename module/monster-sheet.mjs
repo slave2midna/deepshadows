1cb1004bbb02475315369824e19cb39a9e6535c2
Noamancy <69311684+slave2midna@users.noreply.github.com>
2026-04-15 08:33:09 -0500

    Update monster-sheet.mjs

--- module/monster-sheet.mjs
+++ module/monster-sheet.mjs
@@ -1,10 +1,23 @@
-export class MonsterSheet extends foundry.applications.sheets.ActorSheetV2 {
+const { ActorSheetV2 } = foundry.applications.sheets;
+const { HandlebarsApplicationMixin } = foundry.applications.api;
+
+export class MonsterSheet extends HandlebarsApplicationMixin(ActorSheetV2) {
   static DEFAULT_OPTIONS = foundry.utils.mergeObject(super.DEFAULT_OPTIONS, {
     classes: ["deepshadow", "sheet", "actor", "monster-sheet"],
     position: {
       width: 780
+    },
+    form: {
+      closeOnSubmit: false
     }
   }, { inplace: false });
+
+  static PARTS = {
+    sheet: {
+      root: true,
+      template: "systems/deepshadow/templates/monster-sheet.hbs"
+    }
+  };
 
   get title() {
     return this.actor?.name || "Monster";
@@ -12,14 +25,8 @@ export class MonsterSheet extends foundry.applications.sheets.ActorSheetV2 {
 
   async _prepareContext(options) {
     const context = await super._prepareContext(options);
-
     context.actor = this.actor;
     context.levelLabel = "XP";
-
     return context;
-  }
-
-  async _renderHTML(context, options) {
-    return renderTemplate("templates/monster-sheet.hbs", context);
   }
 }
